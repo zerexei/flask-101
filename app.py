17d3f9279b674b530e23164e556fff86c0fa4d9b
Angelo Arcillas <angeloarcillas64@gmail.com>
2023-04-25 21:24:54 +0800

    Add jinja

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def heme():
-    return "Hello Flask!"
+    return render_template('welcome.html')
