610c4398f6760d8df9e56ef7fe2508eb89d0d03e
Angelo Arcillas <angeloarcillas64@gmail.com>
2023-04-25 21:55:12 +0800

    flask routing

--- app.py
+++ app.py
@@ -1,7 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for,request, flash
+import json
+import os.path
+from pprint import pprint
 
 app = Flask(__name__)
+app.secret_key = "secret123"
 
 @app.route('/')
-def heme():
+def home():
+    pprint(request.form)
+    print("asd")
     return render_template('welcome.html')
+
+# TODO: test form input
+@app.post('/url-shorter')
+def url_shorter():
+    urls = {}
+
+    if os.path.exists('urls.json'):
+        with open('urls.json') as urls_file:
+            urls = json.load(urls_file)
+
+    if request.form['code'] in urls.keys():
+        flash("short name already taken")
+        return redirect(url_for('home'))
+    
+    urls[request.form['code']] = {'url': request.form['url']}
+    with open ('urls.json', 'w') as url_file:
+        json.dump(urls, url_file)
+    return "shorter"
+
+@app.get('/redirect-to-home')
+def redirect_to_home():
+    # url_for(<function_name>)
+    return redirect(url_for('home'))
